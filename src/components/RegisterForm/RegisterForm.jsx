0de4c93eabac4e9ae1bcc564e7e579ae7d336c1a
Pedro Oliveira <phr.oliveira08@gmail.com>
2021-12-30 19:51:45 -0300

    add control to form

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,21 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, TextField, Switch, FormControlLabel } from "@mui/material";
 
 const RegisterForm = () => {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [cpf, setCPF] = useState("");
+
+  const [sale, setSale] = useState(true);
+  const [news, setNews] = useState(true);
+
+  const [errors, setErrors] = useState({
+    cpf: {
+      valid: true,
+      message: "",
+    }
+  });
+
+  const functionState = {
+    name: setName,
+    lastname: setLastName,
+    cpf: setCPF,
+    sale: setSale,
+    news: setNews,
+  };
+
+  const handleTextField = (e) => {
+    const { id, value } = e.target;
+    const setValue = functionState[id];
+
+    setValue(value);
+  };
+
+  const handleSwitch = (e) => {
+    const { id, checked } = e.target;
+    const setValue = functionState[id];
+
+    setValue(checked);
+  };
+
+  const validateCPF = () => {
+    let valid = true;
+    let message = "";
+    if (cpf.length !== 11) {
+      valid = false;
+      message = "O CPF precisa ter 11 digitos";
+    }
+    setErrors({
+      ...errors,
+      cpf: {
+        valid,
+        message,
+      }
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("data", {
+      name,
+      lastName,
+      cpf,
+      sale,
+      news,
+    });
+  };
+
   return (
-    <form>
-      <TextField id="name" variant='outlined' label='Nome' fullWidth margin="normal" />
-      <TextField id="lastname" variant='outlined' label='Sobrenome' fullWidth margin="normal" />
-      <TextField id="cpf" variant='outlined' label='CPF' fullWidth margin="normal" />
+    <form onSubmit={submitForm}>
+      <TextField
+        id="name"
+        value={name}
+        onChange={handleTextField}
+        variant="outlined"
+        label="Nome"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="lastname"
+        value={lastName}
+        onChange={handleTextField}
+        variant="outlined"
+        label="Sobrenome"
+        fullWidth
+        margin="normal"
+      />
+      <TextField
+        id="cpf"
+        value={cpf}
+        onBlur={validateCPF}
+        onChange={handleTextField}
+        error={!errors.cpf.valid}
+        helperText={errors.cpf.message}
+        variant="outlined"
+        label="CPF"
+        fullWidth
+        margin="normal"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            id="sale"
+            onChange={handleSwitch}
+            checked={sale}
+            value={sale}
+          />
+        }
+        label="Promoções"
+      />
+      <FormControlLabel
+        control={
+          <Switch
+            id="news"
+            onChange={handleSwitch}
+            checked={news}
+            value={news}
+          />
+        }
+        label="Novidades"
+      />
 
-      <FormControlLabel control={<Switch defaultChecked />} label='Promoções' />
-      <FormControlLabel control={<Switch defaultChecked />} label='Novidades' />
-
-      <Button type="submit" variant='contained'>
+      <Button type="submit" variant="contained">
         Cadastrar
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
